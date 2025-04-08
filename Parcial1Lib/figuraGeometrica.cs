cda3a7a1ab86e67ecf00f9efbd588b3bda52eb0d
Pablo S. <alien.gorrion@gmail.com>
2025-04-07 20:13:51 -0400

    Parcial 1 Pablo Edwin Salas Hidalgo

---
+++ Parcial1Lib/figuraGeometrica.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1Lib
+{
+	public abstract class figuraGeometrica
+	{
+		public abstract double area();
+		public abstract double perimetro();
+
+	}
+
+	public class Triangulo : figuraGeometrica
+	{
+		private double lado;
+		private double altura;
+
+		public Triangulo(double lado, double altura)
+		{
+			this.lado = lado;
+			this.altura = altura;
+		}
+		public override double area()
+		{
+			return (lado * altura) / 2;
+		}
+
+		public override double perimetro()
+		{
+			return lado * 3;
+		}
+
+
+
+	}
+
+	public class Circulo : figuraGeometrica
+	{
+
+		private double radio;
+
+		public Circulo(double radio)
+		{
+			this.radio = radio;
+		}
+		public override double area()
+		{
+
+			return (radio * radio) * Math.PI;
+
+		}
+		public override double perimetro()
+		{
+			return 2 * radio * Math.PI;
+		}
+
+	}
+
+
+
+}
---
+++ Parcial1Test/TestUnitTriangulo.cs
@@ -0,0 +1,31 @@
+﻿using Parcial1Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1Test
+{
+    [TestClass]
+    public sealed class TestUnitTriangulo
+    {
+		[TestMethod]
+		public void TrianguloArea()
+		{
+			Triangulo triangulo = new Triangulo(3, 4);
+			double resultado = triangulo.area();
+			Assert.AreEqual(6, resultado);
+		}
+
+		[TestMethod]
+		public void TrianguloPerimetro()
+		{
+			Triangulo triangulo2 = new Triangulo(2,0);
+			double resultado2 = triangulo2.perimetro();
+			Assert.AreEqual(6, resultado2);
+		}
+
+
+	}
+}
